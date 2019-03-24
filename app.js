9b623dc9f4cacc48991da42913f967b37955b678
rabeehrz <mohammadrabeeh@gmail.com>
2019-03-24 18:59:38 +0530

    SEQUELIZE

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const app = express();
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('data', 'root', 'password', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
 
 app.get('/', (req, res) => {
     res.send("IT WORKS!")
